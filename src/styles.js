2ecdeae8e33b75d2bf6e4fcd7fbedd85d5833204
acaciacornigera <acaciacornigera@hotmail.fr>
2023-03-10 03:13:41 +0400

    Navbar modifications

--- src/styles.js
+++ src/styles.js
@@ -4,14 +4,14 @@ const styles = {
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
-    "font-black text-white lg:text-[55px] sm:text-[50px] xs:text-[40px] text-[30px] lg:leading-[80px] mt-2",
+    "font-black text-white lg:text-[55px] sm:text-[50px] xs:text-[30px] text-[25px] lg:leading-[80px] mt-2",
   heroSubText:
     "text-[#dfd9ff] font-medium lg:text-[25px] sm:text-[23px] xs:text-[18px] text-[18px] lg:leading-[40px]",
   heroSkillsText:
     "text-[#dfd9ff] font-medium lg:text-[25px] sm:text-[25px] xs:text-[18px] text-[20px] lg:leading-[40px]",
 
   sectionHeadText:
-    "text-white font-black md:text-[55px] sm:text-[45px] xs:text-[35px] text-[25px]",
+    "text-white font-black md:text-[55px] sm:text-[45px] xs:text-[30px] text-[25px]",
   sectionSubText:
     "sm:text-[18px] text-[15px] text-secondary uppercase font-black tracking-wider",
 };
